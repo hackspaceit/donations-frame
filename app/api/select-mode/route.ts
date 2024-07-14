120922f9371b1348caaab699d4beebd9c2bf1ef2
Eldar Faaiz <iniakunbisnisku@gmail.com>
2024-07-15 05:28:37 +0700

    upload

---
+++ app/api/select-mode/route.ts
@@ -0,0 +1,73 @@
+import { NextRequest, NextResponse } from "next/server";
+import { getUserInformation, validateFrameRequest } from "@/utils";
+
+const ErrorResponse = `
+<!DOCTYPE html>
+    <html>
+        <head>
+            <meta property="fc:frame" content="vNext" />
+            <meta property="fc:frame:image" content="${process.env.HOST}/assets/images/select-theme.png" />
+            <meta property="og:image" content="${process.env.HOST}/assets/images/select-theme.png" />
+            <meta property="fc:frame:button:1" content="Try Again" />
+            <meta property="fc:frame:post_url" content="${process.env["HOST"]}/api/select-mode" />
+        </head>
+    </html>`;
+
+export async function POST(req: NextRequest): Promise<Response> {
+    // Request from Warpcast
+    const body: { trustedData?: { messageBytes?: string } } = await req.json();
+
+    // Check if frame request is valid
+    const status = await validateFrameRequest(body.trustedData?.messageBytes);
+
+    if (!status?.valid) {
+        console.error("Status", status);
+        return new NextResponse(ErrorResponse);
+    }
+
+    console.log(status);
+
+    // const userInformation = await getUserInformation(fid);
+
+    // console.log(userInformation);
+
+    // if (!userInformation) {
+    //     console.error("userInformation", userInformation);
+    //     return new NextResponse(ErrorResponse);
+    // }
+
+    // const hasUserFollowedMe =
+    //     userInformation?.result?.user?.viewerContext?.following;
+
+    // console.log(fid, hasUserFollowedMe);
+
+    // if (!hasUserFollowedMe) {
+    //     return new NextResponse(`
+    //     <!DOCTYPE html>
+    //     <html>
+    //         <head>
+    //             <meta property="fc:frame" content="vNext" />
+    //             <meta property="fc:frame:image" content="${process.env.HOST}/assets/images/follow.png" />
+    //             <meta property="og:image" content="${process.env.HOST}/assets/images/follow.png" />
+    //             <meta property="fc:frame:button:1" content="Try Again" />
+    //             <meta property="fc:frame:post_url" content="${process.env["HOST"]}/api/select-mode" />
+    //         </head>
+    //     </html>
+    //     `);
+    // }
+
+    return new NextResponse(`
+    <!DOCTYPE html>
+    <html>
+        <head>
+            <meta property="fc:frame" content="vNext" />
+            <meta property="fc:frame:image" content="${process.env.HOST}/assets/images/select-theme.png" />
+            <meta property="og:image" content="${process.env.HOST}/assets/images/select-theme.png" />
+            <meta property="fc:frame:button:1" content="🕴️ Characters" />
+            <meta property="fc:frame:button:2" content="📺 Objects" />
+            <meta property="fc:frame:button:3" content="🦁 Animals" />
+            <meta property="fc:frame:post_url" content="${process.env["HOST"]}/api/start" />
+        </head>
+    </html>
+`);
+}
---
+++ app/page.tsx
@@ -0,0 +1,29 @@
+import { Metadata } from "next";
+
+export const dynamic = "force-dynamic";
+
+export async function generateMetadata(): Promise<Metadata> {
+    return {
+        title: "Mind Reader Farcaster Frame",
+        description: "Mind reading game on Farcaster, follow @harpaljadeja",
+        openGraph: {
+            title: "Mind Reader Farcaster Frame",
+            images: [`${process.env.HOST}/assets/images/first.png`],
+        },
+        metadataBase: new URL(process.env["HOST"] as string),
+        other: {
+            "fc:frame": "vNext",
+            "fc:frame:image": `${process.env.HOST}/assets/images/first.png`,
+            "fc:frame:post_url": `${process.env.HOST}/api/select-mode`,
+            "fc:frame:button:1": "Start",
+        },
+    };
+}
+
+export default function Home() {
+    return (
+        <main className="flex min-h-screen flex-col items-center space-y-4 p-24">
+            <h2>Farcaster Frame that can read your mind</h2>
+        </main>
+    );
+}
